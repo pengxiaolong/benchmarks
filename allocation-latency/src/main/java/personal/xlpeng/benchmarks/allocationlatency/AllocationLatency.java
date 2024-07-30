a190ed0c175a70071e606d1a845907c9b49f6119
Xiaolong Peng <xlpeng@amazon.com>
2024-07-30 16:57:21 -0700

    Add test for data size from 10 bytes to 100_000_000 bytes, output histogram for allocation latency

--- allocation-latency/src/main/java/personal/xlpeng/benchmarks/allocationlatency/AllocationLatency.java
+++ allocation-latency/src/main/java/personal/xlpeng/benchmarks/allocationlatency/AllocationLatency.java
@@ -1,42 +1,77 @@
 package personal.xlpeng.benchmarks.allocationlatency;
 
 import org.HdrHistogram.Histogram;
+import org.HdrHistogram.LinearIterator;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.Instant;
+
 
 public class AllocationLatency {
     static volatile byte[] sink;
-    // A Histogram covering the range from 1 nsec to 1 hour with 3 decimal point resolution:
-    static Histogram histogram = new Histogram(3600000000000L, 3);
+    static long WARMUP_TIME_MSEC = 10000;
+    static long RUN_TIME_MSEC = 15000;
+    static int[] DATA_SIZES = {10, 100, 1000, 10000, 100000, 1_000_000, 10_000_000, 100_000_000};
 
-    static long WARMUP_TIME_MSEC = 5000;
-    static long RUN_TIME_MSEC = 20000;
-
-
-    static void recordTimeToAllocate() {
+    static void recordTimeToAllocate(final int dataSize, final Histogram histogram) {
         long startTime = System.nanoTime();
-        sink = new byte[40000];
+        sink = new byte[dataSize];
         long endTime = System.nanoTime();
         histogram.recordValue(endTime - startTime);
     }
 
-    public static void main(final String[] args) {
+    static Histogram runAllocationTest(final int dataSize) {
+        Histogram histogram = new Histogram(3600000000000L, 3);
+
         long startTime = System.currentTimeMillis();
         long now;
-
         do {
-            recordTimeToAllocate();
+            recordTimeToAllocate(dataSize, histogram);
             now = System.currentTimeMillis();
         } while (now - startTime < WARMUP_TIME_MSEC);
 
         histogram.reset();
 
+        histogram.setStartTimeStamp(System.currentTimeMillis());
         do {
-            recordTimeToAllocate();
+            recordTimeToAllocate(dataSize, histogram);
             now = System.currentTimeMillis();
         } while (now - startTime < RUN_TIME_MSEC);
+        histogram.setStartTimeStamp(System.currentTimeMillis());
 
-        System.out.println("Recorded latencies [in usec] for array allocation:");
-
-        histogram.outputPercentileDistribution(System.out, 1000.0);
+        return histogram;
     }
 
+    public static void main(final String[] args) throws IOException, InterruptedException {
+
+        for (int dataSize : DATA_SIZES) {
+            final Histogram histogram = runAllocationTest(dataSize);
+            final Instant startTime = Instant.ofEpochMilli(histogram.getStartTimeStamp());
+            final Instant endTime = Instant.ofEpochMilli(histogram.getEndTimeStamp());
+            try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(".", "allocation-latency-" + dataSize + "-" + startTime + "-" + endTime + ".csv"), StandardOpenOption.CREATE)) {
+                bufferedWriter.write("Latency(100us), Count");
+                bufferedWriter.newLine();
+                new LinearIterator(histogram, 100_000)
+                        .forEachRemaining(value -> {
+                            if (value.getCountAddedInThisIterationStep() > 0) {
+                                try {
+                                    bufferedWriter.write(String.valueOf((value.getValueIteratedTo() + 1) / 100_000));
+                                    bufferedWriter.write(",");
+                                    bufferedWriter.write(String.valueOf(value.getCountAddedInThisIterationStep()));
+                                    bufferedWriter.newLine();
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                        });
+            }
+
+            System.gc();
+            Thread.sleep(3000);
+        }
+    }
 }
